78870e69d22eadc09fac6b1e85504ddb37b9f025
soilatorome <soila.torome.st@gmail.com>
2023-07-11 19:02:48 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const name = "Joe";
-const height = 74;
+// let count = 0
+const name = "Susan";
+const height = 39
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
